955346139047b24d219832689245ce9023ac9d0f
Aleks <aleksey.ryabovtkd@gmail.com>
2024-07-26 18:42:35 +0300

    Initial commit

---
+++ src/lesson1/MyFirstProgram.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Меня зовут Алексей!\n Я начал учиться с нуля 21.07.2024года.");
+    }
+}
