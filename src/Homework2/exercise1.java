6f15fed78f7d74dbc0355bb411041e26a8840a24
Aleks <aleksey.ryabovtkd@gmail.com>
2024-08-07 16:57:32 +0300

    2

---
+++ src/Homework2/exercise1.java
@@ -0,0 +1,18 @@
+package Homework2;
+
+public class exercise1 {
+    public static void main(String[] args) {
+        String[] words = {"Это", "шашлык", "на", "шампуре"};
+
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) {
+            result += words[i].substring(0);
+            if (i < words.length - 1) {
+                result += "-";
+            }
+        }
+
+        System.out.println(result);
+    }
+}
